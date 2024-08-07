022162742f8ae4690b26a89359ca0bf66827bab4
simranvird <169073573+simranvird@users.noreply.github.com>
2024-08-07 15:41:40 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function hideElements(){
+    const elements= document.getElementsByClassName('text');
+    for(let i=0; i<elements.length;i++ ){
+        elements[i].classList.add('hidden');
+    }
+}
